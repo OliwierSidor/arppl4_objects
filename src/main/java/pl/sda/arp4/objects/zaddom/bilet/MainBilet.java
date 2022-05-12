4918863549febd498e0ad4f95d684dc737368f17
Oliwier <oliwiersidor@gmail.com>
2022-05-12 17:48:33 +0200

    update zaddom

--- src/main/java/pl/sda/arp4/objects/zaddom/bilet/MainBilet.java
+++ src/main/java/pl/sda/arp4/objects/zaddom/bilet/MainBilet.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class MainBilet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Bilet typ = null;
-        for (Bilet rodzaj : Bilet.values()) {
-            System.out.println(rodzaj);
+        RodzajBiletu typ = null;
+        System.out.println("Rodzaje biletu: ");
+        for (RodzajBiletu rodzaj : RodzajBiletu.values()) {
+            System.out.print(rodzaj + ", ");
         }
-
+        System.out.println();
         do {
-            System.out.println("Podaj rodzaj biletu: ");
+            System.out.println("Podaj rodzaj biletu który chcesz: ");
             String rodzajString = scanner.next();
             try {
-                typ = Bilet.valueOf(rodzajString.toUpperCase());
+                typ = RodzajBiletu.valueOf(rodzajString.toUpperCase());
             } catch (IllegalArgumentException iae) {
-                System.err.println("Nie ma takiego biletu");
+                System.out.println("Nie ma takiego biletu");
             }
         } while (typ == null);
 
         System.out.println("Wybrałeś bilet " + typ + " : " + typ.cena + " PLN");
         System.out.println("Ile chciałbyś kupić biletów?");
         int liczbaBiletow = scanner.nextInt();
-        System.out.println("Do zapłaty: " + (liczbaBiletow * typ.cena));
+        System.out.println("Do zapłaty: " + (liczbaBiletow * typ.cena) + " PLN");
     }
 }
 
--- src/main/java/pl/sda/arp4/objects/zaddom/bilet/RodzajBiletu.java
+++ src/main/java/pl/sda/arp4/objects/zaddom/bilet/RodzajBiletu.java
@@ -1,13 +1,13 @@
 package pl.sda.arp4.objects.zaddom.bilet;
 
-public enum Bilet {
+public enum RodzajBiletu {
     NORMALNY(1.70),
     ULGOWY(0.90),
     EMERYT(0.30),
     CALODOBOWY(5.30);
     double cena;
 
-    Bilet(double cena) {
+    RodzajBiletu(double cena) {
         this.cena = cena;
     }
 }
